b5703f1c6987895a4e013a2f985e1eaec279dd39
Wynne Plaga <wynneplaga@gmail.com>
2019-01-24 23:12:26 -0500

    Add missing file and delete dead code

--- app/src/main/java/com/android/databindingbugdemo/ui/main/SubFragment.kt
+++ app/src/main/java/com/android/databindingbugdemo/ui/main/SubFragment.kt
@@ -5,14 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.navArgs
 import com.android.databindingbugdemo.R
 import com.google.android.material.snackbar.Snackbar
 
 class SubFragment : Fragment() {
 
-    private lateinit var viewModel: SubViewModel
     private val arguments: SubFragmentArgs by navArgs()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -22,7 +20,6 @@ class SubFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(SubViewModel::class.java)
 
         Snackbar.make(view!!, "${arguments.flavor.title} costs $${arguments.flavor.cost}!", Snackbar.LENGTH_SHORT).show()
     }
--- app/src/main/java/com/android/databindingbugdemo/ui/main/SubViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.android.databindingbugdemo.ui.main
-
-import androidx.lifecycle.ViewModel;
-
-class SubViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
